8e0809ccec3d81ad1edf1b9ec597c550fa88b442
Ben Reeves <benwolverine2019@gmail.com>
2015-10-30 17:26:57 -0400

    Initial commit

---
+++ Source/MapVisualization/MapVisualizationGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "MapVisualizationGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MAPVISUALIZATION_API AMapVisualizationGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};

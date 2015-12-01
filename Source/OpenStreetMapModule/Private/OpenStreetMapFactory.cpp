09c8b1875d3af4a2b70567f55e3aaafe863b2310
Ben Reeves <bgreeves@umich.edu>
2015-11-30 22:30:23 -0500

    Stubbed out OpenStreetMapFactory

--- Source/OpenStreetMapModule/Private/OpenStreetMapFactory.cpp
+++ Source/OpenStreetMapModule/Private/OpenStreetMapFactory.cpp
@@ -3,6 +3,50 @@
 #include "OpenStreetMapModule.h"
 #include "OpenStreetMapFactory.h"
 
+UOpenStreetMapFactory::UOpenStreetMapFactory()
+{
+    // We don't anybody to create an empty OSM Asset. That's just weird
+    bCreateNew = false;
+}
 
+UOpenStreetMapFactory::~UOpenStreetMapFactory()
+{
 
+}
 
+// Begin UFactory Interface
+
+/** Imports the OpenStreetMapFile from the text of the .osm xml file. */
+UObject* UOpenStreetMapFactory::FactoryCreateText(
+    UClass* InClass,
+    UObject* InParent,
+    FName InName,
+    EObjectFlags Flags,
+    UObject* Context,
+    const TCHAR* Type,
+    const TCHAR*& Buffer,
+    const TCHAR* BufferEnd,
+    FFeedbackContext* Warn) override
+{
+    return nullptr;
+}
+
+/** Returns whether or not the given class is supported by this factory. */
+bool UOpenStreetMapFactory::DoesSupportClass(UClass* Class) override
+{
+    return false;
+}
+
+/** Returns true if this factory can deal with the file sent in. */
+bool UOpenStreetMapFactory::FactoryCanImport(const FString& Filename) override
+{
+    return false;
+}
+
+/** Returns the name of the factory for menus */
+FText UOpenStreetMapFactory::GetDisplayName() const override
+{
+    return FText();
+}
+
+// End UFactory Interface
--- Source/OpenStreetMapModule/Public/OpenStreetMapFactory.h
+++ Source/OpenStreetMapModule/Public/OpenStreetMapFactory.h
@@ -15,5 +15,31 @@ class OPENSTREETMAPMODULE_API UOpenStreetMapFactory : public UFactory
 	GENERATED_BODY()
 	
 public:
+    UOpenStreetMapFactory();
+    virtual ~UOpenStreetMapFactory();
 
+    // Begin UFactory Interface
+
+    /** Imports the OpenStreetMapFile from the text of the .osm xml file. */
+    virtual UObject* FactoryCreateText(
+        UClass* InClass,
+        UObject* InParent,
+        FName InName,
+        EObjectFlags Flags,
+        UObject* Context,
+        const TCHAR* Type,
+        const TCHAR*& Buffer,
+        const TCHAR* BufferEnd,
+        FFeedbackContext* Warn) override;
+
+    /** Returns whether or not the given class is supported by this factory. */
+    virtual bool DoesSupportClass(UClass* Class) override;
+
+    /** Returns true if this factory can deal with the file sent in. */
+    virtual bool FactoryCanImport(const FString& Filename) override;
+
+    /** Returns the name of the factory for menus */
+    virtual FText GetDisplayName() const override;
+
+    // End UFactory Interface
 };
